49d229ea27a297d458888328606591d53e469e4f
AwesomeLemon <a.e.chebykin@gmail.com>
2017-01-23 17:23:10 +0300

    Added python file with code for dataset editing and neural network model construction Tag clean up code.

--- Roslyn_Extract_Methods/MethodsCollector.cs
+++ Roslyn_Extract_Methods/MethodsCollector.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.Win32;
+using Octokit;
 
 namespace Roslyn_Extract_Methods {
     internal class MethodsCollector : CSharpSyntaxWalker {
@@ -39,9 +41,17 @@ namespace Roslyn_Extract_Methods {
                 }
                 var stringComment = summary.Content.ToString();
                 stringComment = Regex.Replace(stringComment, @"\s+", " ", RegexOptions.Multiline).Replace("///", "").Trim();
+                stringComment = CleanUpTags(stringComment);
                 stringComment = removeStuffWithinSuchBrackets(stringComment, '(', ')');
-                stringComment = removeStuffWithinSuchBrackets(stringComment, '<', '>');
                 stringComment = removeStuffWithinSuchBrackets(stringComment, '[', ']');
+                stringComment = removeStuffWithinSuchBrackets(stringComment, '{', '}');
+                var sb = new StringBuilder();
+                foreach (var xmlNodeSyntax in summary.Content) {
+                    if (xmlNodeSyntax is XmlElementSyntax) {
+                        var elementSyntax = (XmlElementSyntax) xmlNodeSyntax;
+                        elementSyntax.Content;
+                    }
+                }
                 if (stringComment.Length < 3) {
                     toRemove.Add(method);
                     continue;
@@ -52,21 +62,36 @@ namespace Roslyn_Extract_Methods {
                 MethodDecls.Remove(methodDeclarationSyntax);
             }
         }
+
+        public static string CleanUpTags(string input) {
+            int start = input.IndexOf('<');
+            string inputbackup = (string) input.Clone();
+            while (start != -1) {
+                int end = input.IndexOf('>');
+                if (end != -1 && start < end) {
+                    var tag = input.Substring(start, end - start + 1);
+                    if (tag.Contains("see cref")) {
+                        var startIndex = tag.IndexOf("cref=\"", StringComparison.Ordinal) + 6;
+                        var lastIndexOf = tag.IndexOf("\"", startIndex, StringComparison.Ordinal);
+                        input = input.Replace(tag, tag.Substring(startIndex, lastIndexOf - startIndex));
+                    }
+                    else input = input.Remove(start, end - start + 1);
+                }
+                else break;
+                start = input.IndexOf('<');
+            }
+            if (input.Contains('<') || input.Contains('>')) {
+                int a = 3;
+            }
+            return input;
+        }
         
         private string removeStuffWithinSuchBrackets(string input, char leftChar , char rightChar) {
             int lbracket = input.IndexOf(leftChar);
             while (lbracket != -1) {
                 var rbracket = input.LastIndexOf(rightChar);
                 if (rbracket != -1 && lbracket < rbracket) {
-                    if (leftChar.Equals('<') && input.Substring(lbracket, rbracket - lbracket + 1).Contains("see cref")) {
-                        var substring = input.Substring(lbracket, rbracket - lbracket + 1);
-                        var startIndex = substring.IndexOf("\"", StringComparison.Ordinal);
-                        var lastIndexOf = substring.LastIndexOf("\"", StringComparison.Ordinal);
-                        var length = lastIndexOf - startIndex - 1;
-                        if (length < 1) input = input.Remove(lbracket, rbracket - lbracket + 1);
-                        else input = input.Replace(substring, substring.Substring(startIndex + 1, length));
-                    }
-                    else input = input.Remove(lbracket, rbracket - lbracket + 1);
+                    input = input.Remove(lbracket, rbracket - lbracket + 1);
                 }
                 else break;
                 lbracket = input.IndexOf(leftChar);
